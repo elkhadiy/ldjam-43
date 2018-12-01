489da034b14aae642945203f2fb7d99a10495ee3
Yassine El Khadiri <yassineelkhadiri@gmail.com>
2018-12-01 14:08:39 +0100

    add level loading from config file

--- ulis43/cli.py
+++ ulis43/cli.py
@@ -3,8 +3,6 @@ from ulis43.game import Game
 
 def run():
     g = Game()
-    print(g.strings)
-
 
 if __name__ == '__main__':
     run()
--- ulis43/game.py
+++ ulis43/game.py
@@ -1,11 +1,30 @@
 import yaml
 import ulis43
+import ulis43.rooms
+import ulis43.spaceship
 
 
 class Game:
 
-    def __init__(self):
-        strings_file = ulis43.basedir / "res" / "strings.yaml"
-        with strings_file.open() as f:
-            strings = yaml.safe_load(f)
-        self.strings = strings
+    def __init__(self, difficulty="easy"):
+
+        res_folder = ulis43.basedir / "res"
+        config_file = res_folder / "config.yaml"
+
+        with config_file.open() as f:
+            config = yaml.safe_load(f)
+        self.config = config
+
+        ship_config = config["difficulty"][difficulty]
+        ressources = ship_config['ressources']
+        # todo: see how to autoload python classes with yaml correctly
+        # !!python/object:ulis43.rooms.Room didn't work
+        rooms = []
+        for room in ship_config['rooms']:
+            rooms.append(ulis43.rooms.Room(**room))
+
+        self.spaceship = ulis43.spaceship.Spaceship(
+            ressources=ressources,
+            crew=None,
+            rooms=rooms
+        )
