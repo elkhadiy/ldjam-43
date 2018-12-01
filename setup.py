8054dbb8d7e01f43a1f04db1bea6b7c77b5069b2
Yassine El Khadiri <yassineelkhadiri@gmail.com>
2018-12-01 11:06:26 +0100

    package setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from ulis43 import __version__
+
+install_requires = [
+    'pygame>=1.9.4'
+]
+
+setup(
+    name='ulis43',
+    version=__version__,
+    description='u lost in space bruh',
+    long_description='yup',
+    author='Jimmy Etienne, Yassine El Khadiri',
+    licence='WTFPL',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    packages=find_packages(exclude=['scripts', 'docs', 'tests']),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'ulis43=ulis43.cli:run'
+        ]
+    }
+)
---
+++ ulis43/cli.py
@@ -0,0 +1,9 @@
+from ulis43.game import Game
+
+
+def run():
+    g = Game()
+
+
+if __name__ == '__main__':
+    run()
