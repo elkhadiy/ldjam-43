3eed115fb16455fb6c6ed9278630cd4c67695faf
Yassine El Khadiri <yassineelkhadiri@gmail.com>
2018-12-03 01:16:18 +0100

    centering ship rooms on the 600x600 square

--- ulis43/game.py
+++ ulis43/game.py
@@ -55,8 +55,24 @@ class Game:
             positions.append(random.sample(adjacents, 1)[0])
             random.shuffle(positions)
 
+        min_x = min(positions, key=lambda p: p[0])[0] * 100
+        min_y = min(positions, key=lambda p: p[1])[1] * 100
+        max_x = max(positions, key=lambda p: p[0])[0] * 100
+        max_y = max(positions, key=lambda p: p[1])[1] * 100
+        center_x = (max_x + 100 - min_x) / 2 + min_x
+        center_y = (max_y + 100 - min_y) / 2 + min_y
+        delta_x = center_x - 300
+        delta_y = center_y - 300
+
+        print(min_x, max_x)
+        print(min_y, max_y)
+        print(center_x, center_y)
+        print(delta_x, delta_y)
+
+        positions = [(p[0] * 100 - delta_x, p[1] * 100 - delta_y) for p in positions]
+
         for i, room in enumerate(rooms):
-            room.pos = tuple(p * 100 for p in positions[i])
+            room.pos = positions[i]
 
         # Crew
 
